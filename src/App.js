12b4db0e1ebab2461acc178cd2b197b5189c615f
Rahul Bhave <bhavemona@gmail.com>
2021-10-09 02:46:32 +0530

    ui bug left

--- src/App.js
+++ src/App.js
@@ -1,22 +1,63 @@
+import axios from 'axios'
 import './App.css';
 import React, {useEffect, useState} from 'react'
+import Clock from 'react-live-clock'
 import {GetData} from './Componant/Api'
 function App() {
-  const [Latitude, setLatitude] = useState();
-  const [Longitude, setLongitude] = useState();
+  const [allValues, setAllValues] = useState({
+    latitude: null,
+    longitude: null,
+    temp: null,
+    locationName: null,
+    country: null
+ });
   
- useEffect(() => {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    setLatitude(position.coords.latitude);
-    setLongitude(position.coords.longitude);
+useEffect(() => {
+  if(navigator.geolocation){
+    getPosition()
+    .then((position) => {
+      console.log(position.coords.latitude)
+      getWeather(position.coords.latitude, position.coords.longitude)
+    })
+  }
+}, [])
+const getPosition = () => {
+  return new Promise(function(resolve, reject)  {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
   });
- }, [])
- const dataapi = GetData(Latitude, Longitude);
+}
+const getWeather = async (Latitude, Longitude) => {
+  const api = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${Latitude}&lon=${Longitude}&units=metric&appid=28a5f601c629788efe8798469d7b9661`
+  ); 
+  const data = await api.json()
+  console.log(data)
+  setAllValues({
+    temp : Math.round(data.main.temp),
+    locationName: data.name,
+    country: data.sys.country
+  })
+}
   return (
-    <div className="App">
-      {
-
-      }
+    <div className="col-8 d-flex justify-content-center py-5">
+      <div className="col-6 app-bg d-flex flex-wrap">
+        <div className="col-12">
+          <h2 className="text-white m-0">{allValues.locationName}</h2>
+          <p className="text-white">IN</p>
+        </div>
+        <div className='col-12 mt-auto d-flex'>
+          <h2 className="text-white m-0">
+            <Clock format={'HH:mm:ss'} ticking={true}/>
+          </h2>
+          <p className="text-white m-0">
+          <Clock format={'dddd, MMMM DD, YYYY'} date={''} />
+          </p>
+        </div>
+        <div className="my-auto">
+          <h1 className="text-white">{allValues.temp}°C</h1>
+        </div>
+        
+      </div>
     </div>
   );
 }
--- src/Componant/Api.js
+++ src/Componant/Api.js
@@ -2,10 +2,11 @@
 import axios from 'axios'
 import { useState, useEffect } from 'react'
 
+
 export const GetData = (Latitude, Longitude) => {
     const [state, setState] = useState();
     useEffect(() => {
-        axios.get(`api.openweathermap.org/data/2.5/weather?lat=${Latitude}&lon=${Longitude}&appid=28a5f601c629788efe8798469d7b9661`)
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${Latitude}&lon=${Longitude}&units=metric&appid=28a5f601c629788efe8798469d7b9661`)
     .then((data) => {
         console.log(data);
     })
